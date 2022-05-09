4d19e938ceb96a7795e5428d8322fd403895449b
SantiCampoamor <101577703+SantiCampoamor@users.noreply.github.com>
2022-05-09 10:42:41 -0300

    cubito clonado con while

---
+++ Assets/Scripts/Instantiator.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Instantiator : MonoBehaviour
+{
+
+    public GameObject objectToClone;
+
+    public void CloneObject()
+    {
+        int a =  1;
+        while (a < 4)
+        {
+            Instantiate(objectToClone);
+            a++;
+        }
+        
+    }
+}
