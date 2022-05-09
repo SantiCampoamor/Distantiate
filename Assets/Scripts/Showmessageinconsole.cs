e4935ae5b8b85519d4f88049be61d0006814dfea
SantiCampoamor <101577703+SantiCampoamor@users.noreply.github.com>
2022-05-09 09:58:43 -0300

    boton hecho

---
+++ Assets/Scripts/Showmessageinconsole.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Showmessageinconsole : MonoBehaviour
+{
+    public void ShowMessage()
+    {
+        Debug.Log("Hello world");
+    }
+}
